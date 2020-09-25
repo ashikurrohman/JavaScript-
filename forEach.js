a9695ee633f7d29e0ff5cddad87dbe50cbad4ad9
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-09-25 11:56:39 +0600

    ForEach

---
+++ forEach.js
@@ -0,0 +1,23 @@
+
+var myArr = [1,2,3,4,5]
+
+myArr.forEach(function(values,indexs,arr){
+	
+	console.log(value,index,arr)
+	
+})
+
+
+
+Result : 
+
+1 0 [ 1, 2, 3, 4, 5 ]
+2 1 [ 1, 2, 3, 4, 5 ]
+3 2 [ 1, 2, 3, 4, 5 ]
+4 3 [ 1, 2, 3, 4, 5 ]
+5 4 [ 1, 2, 3, 4, 5 ]
+
+// 1: We can write anykeyword in anonymous  function aurguments
+
+
+
