fda1c0908287080c18c018175637fc4ddfe8fed0
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-09-30 11:16:09 +0600

    Create TodayDate.js

---
+++ TodayDate.js
@@ -0,0 +1,34 @@
+
+// Find Todays Day = Saturday,Sunday
+
+const date = new Date()
+
+const today = date.getDay()
+
+
+switch(today) {
+    case 0:
+        console.log("Sunday")
+        break  
+    case 1:
+        console.log("Monday")
+        break  
+    case 2:
+        console.log("Tuesday")
+        break  
+    case 3:
+        console.log("Wednesday")
+        break 
+    case 4:
+        console.log("Thusday")
+        break  
+    case 5:
+        console.log("Friday")
+        break
+    default:
+        console.log("Saturday")
+    
+}
+
+
+
