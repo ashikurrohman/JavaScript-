afcaecaedc20a26c40e6b23ecc4b1cb298bb680c
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-10-15 23:42:36 +0600

    Update ObjectAssign.js

--- allMethods/ObjectAssign.js
+++ allMethods/ObjectAssign.js
@@ -1,1 +1,27 @@
-a
+
+
+// Assign Object Property usign Object.assign Method 
+
+
+
+let obj1 = {
+a:1,
+b:2,
+c:3
+}
+let obj3 = {
+g:7,
+h:8
+}
+
+let obj2 = Object.assign({d:4,e:5},obj1);
+console.log(obj2)
+
+
+Result :
+
+a: 1
+b: 2
+c: 3
+d: 4
+e: 5
