fb2d3692e302aadab41f5be6343b41588de6927a
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-10-31 11:10:00 +0600

    Update CallBackFunction.js

--- CallBackFunction.js
+++ CallBackFunction.js
@@ -15,3 +15,22 @@ alert("It's No")
 }
 
 checkFunctionAvailibility("Do you agree?" , yes , no);
+
+
+
+// Print Uppercase and Lowercase using CallBack Function
+
+
+function Tomake(data,callback1,callback2){
+    console.log("This is the main Data : " + data)
+    callback1(data)
+    callback2(data)
+}
+Tomake("Ashikur Rohman",function(data){
+    var upperCase = data.toUpperCase();
+    console.log(upperCase)
+},function(data){
+    var lowercase = data.toLowerCase();
+    console.log(lowercase)
+})
+
