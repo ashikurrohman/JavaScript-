9d22200fa4f196e06faccb1079c08222f076ca88
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-11-17 10:05:36 +0600

    Create restOperator.js

---
+++ 8-Object Oriented JavaScript/restOperator.js
@@ -0,0 +1,6 @@
+const myTest = function(...arguments){
+    for(var i =  0 ; i<arguments.length; i++){
+        console.log("This is the name : " + arguments[i])
+    }
+}
+myTest("Ashik","Boltu","Rohim")
